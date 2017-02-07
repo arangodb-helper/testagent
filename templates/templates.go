91dd830e67eff9f493a5d23698f112eb65ca5315
Ewout Prangsma <ewout@prangsma.net>
2017-02-07 14:01:58 +0100

    Updated to 0.0.2+git

--- templates/templates.go
+++ templates/templates.go
@@ -189,7 +189,7 @@ func templatesGo() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "templates.go", size: 20480, mode: os.FileMode(420), modTime: time.Unix(1486460989, 0)}
+	info := bindataFileInfo{name: "templates.go", size: 20480, mode: os.FileMode(420), modTime: time.Unix(1486472485, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
