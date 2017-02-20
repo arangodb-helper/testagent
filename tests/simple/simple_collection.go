27ca3a9380e6d18ef67d4f84ffed8b563fc60a38
Ewout Prangsma <ewout@prangsma.net>
2017-02-20 10:22:04 +0100

    Handle 404 in remove collection

--- tests/simple/simple_collection.go
+++ tests/simple/simple_collection.go
@@ -45,11 +45,22 @@ func (t *simpleTest) createCollection(c *collection, numberOfShards, replication
 func (t *simpleTest) removeExistingCollection(c *collection) error {
 	operationTimeout, retryTimeout := t.OperationTimeout, t.RetryTimeout
 	t.log.Infof("Removing collection '%s'...", c.name)
-	if _, err := t.client.Delete("/_api/collection/"+c.name, nil, nil, []int{200}, []int{400, 404, 409, 307}, operationTimeout, retryTimeout); err != nil {
+	resp, err := t.client.Delete("/_api/collection/"+c.name, nil, nil, []int{200, 404}, []int{400, 409, 307}, operationTimeout, retryTimeout)
+	if err != nil {
 		// This is a failure
 		t.removeExistingCollectionCounter.failed++
 		t.reportFailure(test.NewFailure("Failed to remove collection '%s': %v", c.name, err))
 		return maskAny(err)
+	} else if resp.StatusCode == 404 {
+		// Collection not found.
+		// This can happen if the first attempt timed out, but did actually succeed.
+		// So we accept this is there are multiple attempts.
+		if resp.Attempts <= 1 {
+			// Not enough attempts, this is a failure
+			t.removeExistingCollectionCounter.failed++
+			t.reportFailure(test.NewFailure("Failed to remove collection '%s': got 404 after only 1 attempt", c.name))
+			return maskAny(fmt.Errorf("Failed to remove collection '%s': got 404 after only 1 attempt", c.name))
+		}
 	}
 	t.removeExistingCollectionCounter.succeeded++
 	t.log.Infof("Removing collection '%s' succeeded", c.name)
