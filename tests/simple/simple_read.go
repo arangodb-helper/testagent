f430cdcd7412fc914fb005a57603d315c2a14d03
Max Neunhoeffer <max@arangodb.com>
2021-04-14 12:45:12 +0200

    Please compiler in tests.

--- tests/simple/simple_read.go
+++ tests/simple/simple_read.go
@@ -49,7 +49,7 @@ func (t *simpleTest) readExistingDocument(
 					if !result.Equals(expected) {
 						// This is a failure
 						t.readExistingCounter.failed++
-						t.reportFailure(test.NewFailure("Read existing document '%s' (%s) returned different values '%s': got %q expected %q", key, ifMatchStatus, c.name, result, expected))
+						t.reportFailure(test.NewFailure("Read existing document '%s' (%s) returned different values '%s': got %v expected %v", key, ifMatchStatus, c.name, result, expected))
 						return "", maskAny(fmt.Errorf("Read returned invalid values"))
 					}
 				}
